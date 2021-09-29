f54116133b217f38ce59f7418c4cca85edf117ef
Adarsh999bh <adarshbhandary48@gmail.com>
2021-09-29 23:30:21 +0530

    roll files added along th text file

---
+++ pay/src/main/java/em/roll/EmployeePayrollFileIOService.java
@@ -0,0 +1,62 @@
+package em.roll;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeePayrollFileIOService {
+	public static final String PAYROLL_FILE_NAME="C:\\Users\\adithya.shenoy_ymedi\\eclipse-workspace\\fileio\\src\\main\\java\\Com\\user\\fileoperation\\fileio\\payroll-file.txt";
+	public void writeData(List<EmployeePayrollData> employeePayrollList) {
+		StringBuffer empBuffer=new StringBuffer();
+		employeePayrollList.forEach(employee->{
+			String emloyeeDataString=employee.toString().concat("\n");
+			empBuffer.append(emloyeeDataString);     
+		});
+		try {
+			Files.write(Paths.get(PAYROLL_FILE_NAME),empBuffer.toString().getBytes());
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public long countEntries() {
+		long entries=0;
+		try {
+			entries=Files.lines(new File(PAYROLL_FILE_NAME).toPath()).count();
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		return entries;
+	}
+
+	public void printData() {
+		try {
+			Files.lines(new File(PAYROLL_FILE_NAME).toPath())
+			.forEach(System.out::println);   
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public List<EmployeePayrollData> readData(){
+        List<EmployeePayrollData> employeePayrollList=new ArrayList<>();
+        try {
+            Files.lines((new File(PAYROLL_FILE_NAME).toPath()))
+            .map(line->line.trim())
+            .forEach(line->{
+                String[] s=line.split(",");
+                employeePayrollList.add(new EmployeePayrollData(Integer.parseInt(s[0]),s[1],Double.parseDouble(s[2])));
+            });
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+        return employeePayrollList;
+    }
+
+}
